00ad233faf37bcc1d0005976900a4d9f4e8f7ab9
emily_liu <yingpik67@hotmail.com>
2018-07-25 01:17:05 +0800

    pricing table unfinish

--- app/views/pricing.php
+++ app/views/pricing.php
@@ -1,21 +1,23 @@
 <?php
  defined('BASEPATH') OR exit('No direct script access allowed');
 $pricing_lang = $this->lang->line('pricing');
-
 ?>
-<table>
-	<?php foreach($pricing_lang['header'] as $cell): ?>
-		<th>
-			<td>
-				<?php echo $cell; ?>
-			</td>
-		</th>
-	<?php endforeach; ?>
-	<?php foreach($pricing_lang['data'] as $row): ?>
-		<tr>
-			<td>
-				<?php echo $row ;?>
-			</td>
-		</tr>
-	<?php endforeach; ?>
-</table>
+<link href="<?php base_url();?>/src/css/pricing.css" rel="stylesheet" />
+<section id="priciing" class="pricing">
+	<table>
+		<?php foreach($pricing_lang['header'] as $cell): ?>
+			<th>
+				<td>
+					<?php echo $cell; ?>
+				</td>
+			</th>
+		<?php endforeach; ?>
+		<?php foreach($pricing_lang['data'] as $row): ?>
+			<tr>
+				<td>
+					<?php echo $row ;?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+</section>
