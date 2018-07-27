3405b010cb08c97d6a4b711162ce5d9ee2020b74
emily_liu <yingpik67@hotmail.com>
2018-07-28 02:02:16 +0800

    track, profile,exp,contact pages
    track, profile,exp,contact pages for mobile version

--- app/views/profile.php
+++ app/views/profile.php
@@ -11,10 +11,10 @@ $profile_lang = $this->lang->line('profile');
             </div>
         </div>
         <div class="row">
-            <div class="col-6">
+            <div class="col-lg-6 col-sm-12">
                 <img class='w-100 pro_img' src='<?php base_url();?>/src/image/profile/profile.png'>
             </div>
-            <div class="col-6">
+            <div class="col-lg-6 col-sm-12">
                 <div class="profile-title">
                     <h1><?php echo $profile_lang['title']?></h1>
                 </div>
