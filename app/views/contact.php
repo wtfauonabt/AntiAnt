12c70a988ab8695cf6fcd82551d8077f49c06da6
Stanley Yeung <stanleyhahaha@gmail.com>
2018-07-28 15:06:49 +0800

    Revert "Revert "Merge branch 'features/A006-UAT-Test-Refine'""
    This reverts commit b6ee0948585ed0cd7266dc0cc9829b9938549ea9.

--- app/views/contact.php
+++ app/views/contact.php
@@ -39,17 +39,43 @@ $address = 'Hang+Wai+Industrial+Center,6+Kin+Tai+Street,Tuen+Mun,N.T.,Hong+Kong'
         
         <div class="row">
             <div class="col-lg-6 col-sm-12">
-                <!--<div id="map" class='map'>My map will go here</div>-->
-                <iframe width="100%" height="100%" rameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=<?php echo $api_key; ?>&q=<?php echo $address; ?>"></iframe>
-                <!--<img class='w-100 about_img' src='<?php base_url();?>/src/image/map.png'>-->
-                 <p style="text-align: center;"><?php echo $contact_lang['address']?></p>
+				<?php if($current_lang == "english"): ?>
+					<!--<div id="map" class='map'>My map will go here</div>-->
+                	<iframe width="100%" height="100%" rameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=<?php echo $api_key; ?>&q=<?php echo $address; ?>"></iframe>
+                	<!--<img class='w-100 about_img' src='<?php base_url();?>/src/image/map.png'>-->
+				<?php else: ?>
+					<?php $this->load->view("baidu"); ?>
+				<?php endif; ?>
+
             </div>
             <div class="col-lg-6 col-sm-12 contact_position">
-                <p><?php echo $contact_lang['web']?></p>
-                <p><?php echo $contact_lang['email']?></p>
-                <p><?php echo $contact_lang['number']?></p>
-                <p><?php echo $contact_lang['openhours']?></p>
-
+				<div class="table-responsive">
+					<table class="table">
+						<tbody>
+						<?php foreach($contact_lang['header'] as $key => $value): ?>
+						<tr>
+							<th><?php echo $value; ?></th>
+							<?php if($key == 'open_hours'): ?>
+								<td>
+								<?php foreach($contact_lang['content'][$key] as $content): ?>
+									<p><?php echo $content; ?></p>
+								<?php endforeach; ?>
+								</td>
+							<?php else: ?>
+								<td><?php echo $contact_lang['content'][$key]; ?></td>
+							<?php endif; ?>
+						</tr>
+						<?php endforeach; ?>
+						<tr>
+							<th scope="row"></th>
+							<td>
+								<a class="wechat btn btn-lg" href="weixin://dl/chat?AAlogistic">
+									<i class="fab fa-weixin"></i>
+								</a>
+							</td>
+						</tbody>
+					</table>
+				</div>
             </div>
         </div>
     </div>
