ce77e4ef8c7105af685d455d155349cd80bb30ef
emily_liu <yingpik67@hotmail.com>
2018-08-02 00:30:50 +0800

    logo & tracking page
    logo background del
    tracking box changed

--- app/views/tracking.php
+++ app/views/tracking.php
@@ -4,38 +4,41 @@ $tracking_lang = $this->lang->line('tracking');
 ?>
 <link href="<?php base_url();?>/src/css/tracking.css" rel="stylesheet" />
 <section id="tracking">
-    <div class="container"style="background-color: #3366ff">
+    <div class="container bg">
         
         <div class="row">
-            <div class="col-6" >
+            <div class="col-6 bg_left" >
                 <form>
-                  <div class="form-group">
-                    <label><?php echo $tracking_lang['tracking_number']; ?></label>
-                    <div >
+                  <div class="form-group in_out_put">
+                    <div class="label_hig_mob">
+                      <label><?php echo $tracking_lang['tracking_number']; ?></label>
+                    </div>
                       <div class="form-group input_box">
                       
                            <input class="form-control" type="text" placeholder="EK12345678" aria-label="<?php echo $tracking_lang['tracking_number']; ?>">
                       </div>
-                    </div>
+                    
 
                  </div>
+               </form> 
+            </div>
+            <div class="col-6 bg_right ">
+              <form>
+                <div class="form-group in_out_put" >
+                  <div>
+                    <label><?php echo $tracking_lang['status']; ?></label>
+                  </div>
+                    <div class="form-group input_box output_box">
+                      <input class="form-control" type="text" placeholder="" aria-label="<?php echo $tracking_lang['tracking_number']; ?>">
+                    </div>
                 
             </div>
-            <div class="col-6">
-              <div class="status" >
-                <label><?php echo $tracking_lang['status']; ?></label>
-              </div>
-              <div class="output_box">
-               <p> </p>
-              </div>
-            </div>
-            <div class="submit text-center">
-               <button class="btn  submit_btn" type="submit"><?php echo $tracking_lang['submit']; ?></button>
-            </div>
-            </form>
+                  </form>
         </div>
-    </div>
-    
-    
+        </div>
+        <div class="sub_btn" >
+                  <button class="" type="submit"><?php echo $tracking_lang['submit']; ?></button>
+                </div>
+    </div>  
 </section>
 <script src="<?php base_url();?>/src/js/tracking.js"></script>
