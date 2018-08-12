8dec6b596985c1003ace4866ad9ec3e9c34caaac
wtfauonabt <stanleyhahaha@gmail.com>
2018-08-12 13:07:38 +0800

    fix missing nav link

--- app/language/english/base_lang.php
+++ app/language/english/base_lang.php
@@ -37,7 +37,8 @@ $lang["nav"]["header"]["support"] 		= "Support";
 // Dropdowns Langugage
 $lang["nav"]["quick"]["tracking"]		= "Track & Trace";
 //$lang["nav"]["quick"]["faq"]			= "FAQs";
-$lang["nav"]["quick"]["contact"]		= "Contact Us Profile";
+$lang["nav"]["quick"]["contact"]		= "Contact Us";
+$lang["nav"]["about"]["profile"]		= "Profile";
 $lang["nav"]["about"]["history"]		= "Company History";
 $lang["nav"]["service"]["expertise"]	= "Expertise";
 $lang["nav"]["support"]["contact"]		= "Contact Us";
