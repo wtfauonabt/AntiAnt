14bad25d7e68fe49409f62c0a57597b3c18cb905
emily_liu <yingpik67@hotmail.com>
2018-08-16 09:40:49 +0800

    login function

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -6,7 +6,40 @@
  * Time: 9:36 PM
  */
 
-class UserController
-{
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class UserController extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('UserModel');
+	}
+
+	public function handle($action){
+		if(!$action){
+			throw new Exception("No Action");
+		}
+		switch ($action){
+			case('login'):
+				$this->login();
+				break;
+			case('signout'):
+				break;
+			default:
+				throw new Exception("Cannot find action");
+		}
+		
+		//view login
+	}
+
+
+	public function login(){
+		// if(!) check if coming back is post 
+		$data["user_name"] = $this->input->post('user_name');
+		$data["password"] = $this->input->post('password');
+
+		$error_message = $this->UserModel->login($data);
+
+		return $error_message;
+	}
 
 }
--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -5,8 +5,42 @@
  * Date: 8/15/2018
  * Time: 9:37 PM
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class UserModel
+class UserModel extends CI_Model
 {
+	private $table_name;
 
+	public function __construct(){
+        parent::__construct();
+        $this->table_name = "account_user";
+    }
+
+    public function login($data){
+    	// check error
+    	$errorMessge = $this->checkUser($data["user_name"], $data["user_name"]);
+    	return $errorMessage;
+    }
+
+    public function checkUser($user_name, $password){
+    	$encrpted_password = $this->encryptPassword($user_name, $password);
+
+    	$this->load->db("default");
+
+    	$sql = "SELECT * FROM {$this->table_name} WHERE user_name='{$user_name}, password={$encrpted_password}"
+    	//$result = query
+    	//$result = query to array
+    	if(count($result)) != 1){
+			return "Invalid username and password";
+		}
+		return NULL;
+    }
+
+    public function encryptPassword($user_name, $password){
+    	return md5($user_name) + md5($password);
+    }
+
+    public function decryptPassword($user_name, $password){
+
+    }
 }
