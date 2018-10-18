8e47fb4a885839c878e6bfde7f5020770d7975a2
emily_liu <yingpik67@hotmail.com>
2018-10-18 17:45:53 +0800

    login page style

--- app/views/login_page.php
+++ app/views/login_page.php
@@ -4,25 +4,41 @@
 ?>
 <link href="<?php base_url();?>/src/css/login.css" rel="stylesheet" />
  <section class="login-page" id="login_page">
-	<img src="<?php echo base_url();?>/src/image/logo.png">
+
+ 	<div class="limiter">
+		<div class="container-login100" style="background-color: blue">
+			<div class="wrap-login100">
+				<span class="login100-form-logo">
+	<img src="<?php echo base_url();?>/src/image/logo.png"></span>
 	<form class=" animate input_box" method="post" action="<?php echo site_url("/UserController/handle/login");?>">
 		<div class="container ">
-		<label for="uname"><b><?php echo $contact_lang['username']?></b></label>
-		<input type="text" placeholder="Enter Username" name="user_name" required>
 
-		<label for="psw"><b><?php echo $contact_lang['password']?></b></label>
-		<input type="password" placeholder="Enter Password" name="password" required>
-
-		<button type="submit">Login</button>
-		<label>
-		<input type="checkbox" checked="checked" name="remember"> Remember me
-		</label>
+			<div class="wrap-input100 validate-input" >
+		<label class="input100" for="uname"><b><?php echo $contact_lang['username']?></b></label>
+		<input class="input100" type="text" name="user_name" required>
+			</div>
+		<div class="wrap-input100 validate-input" >
+		<label class="input100" for="psw"><b><?php echo $contact_lang['password']?></b></label>
+		<input class="input100" type="password"  name="password" required>
+	</div>
+	<div class="">
+		<input  type="checkbox" checked="checked" name="remember"><span style="color: #fff"> Remember me
+		</span> </label>
+	</div>
+	
+		<div class="container-login100-form-btn">
+		<button class="login100-form-btn"type="submit">Login</button>
+</div>
+			
 		</div>
 
-		<div class="container">
-		<span class="psw">Forgot <a href="#">password?</a></span>
+		<div class="text-center p-t-90">
+		<spa nclass="psw"><a class="txt1" href="#">Forgot password?</a></span>
 		</div>
  
 	</form>
+</div>
+</div>
+</div>
  </section>
  <script></script>
